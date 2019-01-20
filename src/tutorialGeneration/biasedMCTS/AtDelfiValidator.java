3d565cf5714259026db68a4db682d3420171dc98
Michael Green <mcg520@nyu.edu>
2019-01-20 17:17:18 -0500

    adding biased MCTS

---
+++ src/tutorialGeneration/biasedMCTS/AtDelfiValidator.java
@@ -0,0 +1,95 @@
+package tutorialGeneration.biasedMCTS;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
+import core.competition.CompetitionParameters;
+import core.game.Game;
+import core.game.StateObservation;
+import core.vgdl.VGDLFactory;
+import core.vgdl.VGDLParser;
+import core.vgdl.VGDLRegistry;
+import ontology.Types;
+import tools.Utils;
+import tracks.ArcadeMachine;
+import video.constants.InteractionStaticData;
+
+public class AtDelfiValidator {
+
+	public boolean VERBOSE = true;
+	String atDelfiGamesCollection =  "examples/atDelfi_games.csv";
+    String gamesPath = "examples/gridphysics/";
+    String physicsGamesPath = "examples/contphysics/";
+    String generateTutorialPath = gamesPath;
+
+	String[][] games = Utils.readAtDelfiGames(atDelfiGamesCollection);
+    int levelIdx = 0; // level names from 0 to 4 (game_lvlN.txt).
+    int gameIdx = 47;
+	
+	public StateObservation startup(int randomSeed, String game_file, String level_file) throws IOException {
+		VGDLFactory.GetInstance().init(); // This always first thing to do.
+		VGDLRegistry.GetInstance().init();
+
+		if (VERBOSE)
+			System.out.println(" ** Playing game " + game_file + ", level " + level_file + " **");
+
+		if (CompetitionParameters.OS_WIN)
+		{
+			System.out.println(" * WARNING: Time limitations based on WALL TIME on Windows * ");
+		}
+
+		// First, we create the game to be played..
+		Game toPlay = new VGDLParser().parseGame(game_file);
+		toPlay.buildLevel(level_file, randomSeed);
+
+		if(InteractionStaticData.saveSpriteGroup) {
+			InteractionStaticData.createJSONInfo(toPlay);
+		}
+		
+		// Warm the game up.
+		ArcadeMachine.warmUp(toPlay, CompetitionParameters.WARMUP_TIME);
+		
+		return toPlay.getObservation();
+	}
+	public static void main(String[] args) {
+
+		int seed = 100;
+		AtDelfiValidator validator = new AtDelfiValidator();
+		
+		String gameFile = validator.generateTutorialPath + validator.getGame(validator.gameIdx)[1] + ".txt";
+		String levelFile = validator.gamesPath + validator.getGame(validator.gameIdx)[1] + "_lvl" + validator.levelIdx + ".txt";
+		
+		try {
+			StateObservation root = validator.startup(seed, gameFile, levelFile);
+			
+			// more setup stuff
+	        ArrayList<Types.ACTIONS> act = root.getAvailableActions();
+	        Types.ACTIONS[] actions = new Types.ACTIONS[act.size()];
+	        
+	        for(int i = 0; i < actions.length; ++i)
+	        {
+	            actions[i] = act.get(i);
+	        }
+	        
+	        int num_actions = actions.length;
+	        
+	        boolean improved = true;
+	        
+	        SingleMCTSPlayer player = new SingleMCTSPlayer(new Random(seed), num_actions, actions, improved);
+	        
+	        player.init(root);
+	        player.run();
+	        
+	        System.out.println("Complete");
+	        
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public String[] getGame(int id) {
+		return games[id];
+	}
+}
